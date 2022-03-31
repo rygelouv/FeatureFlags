11aeb1a4d6046aeb0f2968c1b71aa44c6b328a8b
Rygel Louv <quentinlouv@gmail.com>
2022-03-31 20:28:50 +0000

    Adding documentation to README.md

--- README.md
+++ README.md
@@ -12,7 +12,16 @@ This work is inspired and is an update and improvement (in my opinion) of the or
 - TestFeatureFlagProvider moved to test source set and configured to easily enable or disable feature but still used with FeatureManager
 - FeatureManager will be used in Test but a different instance will be injected. The test instance will remove all the other providers and keep only TestFeatureFlagProvider
 
+## Screenshots
 <p align="center">
 <img src="https://github.com/rygelouv/FeatureFlags/blob/main/screenshot-1648756729152.png" width="200">   <img src="https://github.com/rygelouv/FeatureFlags/blob/main/screenshot-1648756740397.png" width="200">   <img src="https://github.com/rygelouv/FeatureFlags/blob/main/screenshot-1648756748689.png" width="200">
 </p>
 
+## Code details
+- A Feature uniquely identifies a part of the app code that can either be enabled or disabled.
+Features only have two states by design to simplify the implementation
+- Every provider has an explicit priority so they can override each other (e.g. "Remote Config tool" > Store).
+Not every provider has to provide a flag value for every feature. This is to avoid implicitly relying on build-in
+defaults (e.g. "Remote Config tool" returns false when no value for a feature) and to avoid that every provider has to provide a
+value for every feature. (e.g. no "Remote Config tool" configuration needed, unless you want the toggle to be remote)
+  
--- app/src/main/java/com/example/featureflagdebugmenu/ui/UIContract.kt
+++ app/src/main/java/com/example/featureflagdebugmenu/ui/UIContract.kt
@@ -1,29 +1,48 @@
 package com.example.featureflagdebugmenu.ui
 
+/**
+ * Represents the routes to the different screens of our debug menu UI
+ */
 enum class Route(val routeName: String) {
     CATALOGS("flags_catalog"),
     FLAGS("feature_flags"),
     CONFIRMATION_DIALOG("confirmation_dialog")
 }
 
+/**
+ * Represents a screen of the debug menu UI
+ */
 sealed class FeatureFlagScreen(open val route: Route) {
 
+    /**
+     * Displays the List of Flags catalog
+     */
     data class CatalogScreen(
         override val route: Route = Route.CATALOGS,
         val catalogs: List<CatalogUi> = listOf(),
     ) : FeatureFlagScreen(route)
 
+    /**
+     * Displays the list of feature flags
+     */
     data class FlagScreen(
         override val route: Route = Route.FLAGS,
         val flags: List<FeatureUi> = listOf(),
         val isButtonEnabled: Boolean = false
     ) : FeatureFlagScreen(route)
 
+    /**
+     * Displays the confirmation Alert Dialog
+     */
     data class ConfirmationDialog(
         override val route: Route = Route.CONFIRMATION_DIALOG,
     ) : FeatureFlagScreen(route)
 }
 
+/**
+ * UI state being dispatched to the UI side. Compose screens (composables) will consume this UI
+ * State in order to trigger recompositions
+ */
 data class FeatureFlagUiState(
     val catalogScreen: FeatureFlagScreen = FeatureFlagScreen.CatalogScreen(),
     val flagsScreen: FeatureFlagScreen = FeatureFlagScreen.FlagScreen(),
@@ -31,11 +50,17 @@ data class FeatureFlagUiState(
     val title: String = ""
 )
 
+/**
+ * Single shot ViewModel events used for Navigation and singe shot events like Snack bars
+ */
 data class FeatureFlagEvents(
     val showDialog: Boolean = false,
     val restartApp: Boolean = false
 )
 
+/**
+ * Represent an action performed by the user on UI side
+ */
 sealed class FeatureFlagAction {
     data class CatalogClickedAction(val catalog: CatalogUi) : FeatureFlagAction()
     data class FeatureUpdatedAction(
@@ -47,6 +72,9 @@ sealed class FeatureFlagAction {
     object SaveUpdates : FeatureFlagAction()
 }
 
+/**
+ * UI model for a [Feature]
+ */
 data class FeatureUi(
     val key: String,
     val name: String,
@@ -54,6 +82,9 @@ data class FeatureUi(
     var isEnabled: Boolean
 )
 
+/**
+ * UI model for a Catalog
+ */
 enum class CatalogUi(
     val label: String,
     val description: String
